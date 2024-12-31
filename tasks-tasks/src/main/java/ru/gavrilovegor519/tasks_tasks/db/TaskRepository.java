43cad1e7490d333e9994660d75594a256f6c1ba0
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2024-12-31 21:17:49 +0300

    Migrate to PostgreSQL.

--- tasks-tasks/src/main/java/ru/gavrilovegor519/tasks_tasks/db/TaskRepository.java
+++ tasks-tasks/src/main/java/ru/gavrilovegor519/tasks_tasks/db/TaskRepository.java
@@ -2,8 +2,8 @@ package ru.gavrilovegor519.tasks_tasks.db;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends MongoRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
     Page<Task> findAllByAuthorEmail(String authorEmail, Pageable pageable);
 }
